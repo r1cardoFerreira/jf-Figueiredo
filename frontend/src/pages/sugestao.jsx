474d3045d9b1db49b3a0312dbf69f9ebef706bbd
r1cardoFerreira <ricardoferreira1510@outlook.pt>
2025-07-03 10:48:10 +0100

    Form de sugestoes sem backend algum

---
+++ frontend/src/pages/sugestao.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import"../styles/sugestao.css"
+
+const Sugestao =() =>{
+    return(
+        <div>
+            <Navbar/>
+        <div className="sugestao-container">
+            <h2 className="sugestao-tittle">Sugestao</h2>
+            <form>
+                <span>Nome*</span>
+                <input type="text" className="sugestao-input"></input>
+                <span>Email*</span>
+                <input type="text" className="sugestao-input"></input>
+                <span>Assunto*</span>
+                <input type="text" className="sugestao-input"></input>
+                <span>Mensagem*</span>
+                <textarea className="sugestao-input msg"></textarea>
+                <button type="submit" className="sugestao-button">Enviar</button>
+            </form>
+        </div>
+        </div>
+    )
+}
+export default Sugestao;
